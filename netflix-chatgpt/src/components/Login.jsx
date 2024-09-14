a026391582e0689001ca22f3bd45be0073e2ddc3
Mayank Diwakar <mayank.diwakar@styldod.com>
2024-09-14 14:19:19 +0530

    Add SignUp

--- netflix-chatgpt/src/components/Login.jsx
+++ netflix-chatgpt/src/components/Login.jsx
@@ -1,6 +1,13 @@
 import Header from "./Header";
+import { useState } from "react";
 
 const Login = () => {
+    const [isSignInForm, setIsSignInForm] = useState(true);
+    const toggleSignInForm = () => {
+        setIsSignInForm(!isSignInForm);
+        console.log(isSignInForm);
+    };
+
     return (
         <div>
             <Header />
@@ -11,20 +18,35 @@ const Login = () => {
                 />
             </div>
             <form className="p-12 bg-black absolute w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-                <h1 className="font-bold text-3xl py-4">Sign In</h1>
+                <h1 className="font-bold text-3xl py-4">
+                    {isSignInForm ? "Sign In" : "Sign Up"}
+                </h1>
+
+                {!isSignInForm && (
+                    <input
+                        type="text"
+                        placeholder="Full Name"
+                        className="p-2 my-3 w-full bg-gray-700"
+                    />
+                )}
                 <input
                     type="text"
                     placeholder="Email"
-                    className="p-2 my-2 w-full bg-gray-700"
+                    className="p-2 my-3 w-full bg-gray-700"
                 />
                 <input
                     type="password"
                     placeholder="Password"
-                    className="p-2 my-2 w-full bg-gray-700"
+                    className="p-2 my-3 w-full bg-gray-700"
                 />
-                <button className="p-2 my-2 bg-red-700 w-full rounded-lg">
-                    Sign In
+                <button className="p-2 my-6 bg-red-700 w-full rounded-lg">
+                    {isSignInForm ? "Sign In" : "Sign Up"}
                 </button>
+                <p className="cursor-pointer" onClick={toggleSignInForm}>
+                    {isSignInForm
+                        ? "New to Netflix? Sign Up Now"
+                        : "Already have an account? Sign In"}
+                </p>
             </form>
         </div>
     );
